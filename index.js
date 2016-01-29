55fce40decba9b501cf8f4e139f05941f9aae6f0
cheekyshroom <phernima@ucsc.edu>
2016-01-29 15:58:31 -0800

    added some requires

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var self = require('sdk/self');
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+var pageMod = require("sdk/page-mod");
 
-// a dummy function, to show how tests work.
-// to see how to test this function, look at test/test-index.js
-function dummy(text, callback) {
-  callback(text);
-}
-
-exports.dummy = dummy;
