1a3a02113980e1461b1feccc6be6b41b2d757981
cheekyshroom <phernima@ucsc.edu>
2016-01-30 11:33:08 -0800

    Nice password things now.. fam

--- data/popup.js
+++ data/popup.js
@@ -1,15 +1,30 @@
-
 $(document).ready(function(){
   $("#password").click(function(){
     $("#message").html("<input type=\"password\" id=\"entry\">");
+    var password = null;
     $("#entry").keyup(function(e){
       if (e.keyCode == 13){
+        password = $("#entry").val();
         chrome.runtime.sendMessage({option: "set_password", data: $("#entry").val()}, function(response){
           console.log("response: "+response);
         });
-        $("#message").html("<input 
+        $("#message").append("<button type=button id=lock>Lock</button>");
+        $("#lock").click(function(){
+          $("#everything").hide();
+          $("#above").html("<button type=button id=unlock>Unlock</button>");
+          $("#unlock").click(function(){
+            $("#above").html("<p>Enter your password to unlock</p><input type=\"password\" id=unlockpw>");
+            $("#unlockpw").keyup(function(e){
+              if (e.keyCode == 13){
+                if ($("#unlockpw").val() == password){
+                  $("#everything").show();
+                  $("#above").html("");
+                }
+              }
+            });
+          });
+        });
       }
     });
   });
-
 });
