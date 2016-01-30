523b1032424ed1092903c2834a06e806332384ea
cheekyshroom <phernima@ucsc.edu>
2016-01-29 16:01:17 -0800

    Added more boilerplate

--- index.js
+++ index.js
@@ -3,3 +3,13 @@ var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 var pageMod = require("sdk/page-mod");
 
+var button = buttons.ActionButton({
+   id: "mozilla-link",
+   label: "FBSecure",
+   icon: "./icon-16.png",
+   onClick: handleClick,
+});
+
+function handleClick(state){
+   
+}
