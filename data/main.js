f44643966279a0cf87ab6e77aef9f401179843a2
Ray <rchen25@ucsc.edu>
2016-01-29 19:47:06 -0800

    Removed RSA test code

--- data/main.js
+++ data/main.js
@@ -1,27 +1,3 @@
-var PassPhrase = "This is a testing passphrase.";
-var Bits = 1024;
-
-console.log(PassPhrase);
-
-var myRSAKey = cryptico.generateRSAKey(PassPhrase, Bits);
-var myPublicKeyString = cryptico.publicKeyString(myRSAKey);
-
-console.log(myPublicKeyString);
-
-var messageString = "This is an example message.";
-
-console.log(messageString);
-
-var encryptedMessage = cryptico.encrypt(messageString, myPublicKeyString);
-var cipherMessage = encryptedMessage.cipher;
-
-console.log(cipherMessage);
-
-var decryptedMessage = cryptico.decrypt(cipherMessage, myRSAKey);
-var decryptedMessageString = decryptedMessage.plaintext;
-
-console.log(decryptedMessageString);
-
 console.log("RUUUUN");
 
 function keyDown() {
