24afbb1dd6a99a07eeef2ac661e5ee6525a4244e
Ray <rchen25@ucsc.edu>
2016-01-29 19:40:31 -0800

    RSA stuff

--- data/main.js
+++ data/main.js
@@ -1,3 +1,27 @@
+var PassPhrase = "This is a testing passphrase.";
+var Bits = 1024;
+
+console.log(PassPhrase);
+
+var myRSAKey = cryptico.generateRSAKey(PassPhrase, Bits);
+var myPublicKeyString = cryptico.publicKeyString(myRSAKey);
+
+console.log(myPublicKeyString);
+
+var messageString = "This is an example message.";
+
+console.log(messageString);
+
+var encryptedMessage = cryptico.encrypt(messageString, myPublicKeyString);
+var cipherMessage = encryptedMessage.cipher;
+
+console.log(cipherMessage);
+
+var decryptedMessage = cryptico.decrypt(cipherMessage, myRSAKey);
+var decryptedMessageString = decryptedMessage.plaintext;
+
+console.log(decryptedMessageString);
+
 console.log("RUUUUN");
 function run() {
   console.log("run");
@@ -7,9 +31,6 @@ function run() {
       return;
     console.log(name);
   });
-
-  var MattsRSAkey = cryptico.generateRSAKey(PassPhrase, Bits);
-  
 }
 
 setInterval(run, 5000);
