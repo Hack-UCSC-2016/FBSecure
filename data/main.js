fd88dfab4c4e59cf44fb2079c15a26730ffb649d
Cole Faust <colecfaust@gmail.com>
2016-01-29 19:39:39 -0800

    Reading messages and fake textbox

--- data/main.js
+++ data/main.js
@@ -1,4 +1,9 @@
 console.log("RUUUUN");
+
+function keyDown() {
+  console.log('Keydown');
+}
+
 function run() {
   console.log("run");
   $('.fbDockWrapper .fbNub').each(function() {
@@ -6,6 +11,24 @@ function run() {
     if(name.length === 0)
       return;
     console.log(name);
+
+    if($(this).find('textarea').length === 1) {
+      var ta = $(this).find('textarea');
+      ta.hide();
+      var newTa = $('<textarea title="Type an encrypted message..." placeholder="Type an encrypted message..." style="height: 14px;"></textarea>').attr('class', ta.attr('class')).keyup(function(e) {
+        if(e.keyCode === 13) {
+          ta.val(newTa.val()+" aslkdfjasfdkljea");
+          ta.trigger("keyup", {keyCode: 13});
+          newTa.val("");
+        }
+      });;
+      ta.parent().append(newTa);
+    }
+    
+    var messages = $(this).find('div.conversation > div > div:last-child > div');
+    messages.each(function() {
+      console.log($(this).text());
+    });
   });
 }
 
