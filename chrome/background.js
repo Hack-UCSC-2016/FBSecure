5dfc3fa0b3924562d3c3677822bcc94eb9c1c9ae
cheekyshroom <phernima@ucsc.edu>
2016-01-29 21:17:30 -0800

    Updated background and popup (useless)

--- chrome/background.js
+++ chrome/background.js
@@ -1,7 +1,25 @@
+var users = {};
+
+function saveUser(name, key){
+  
+}
+
+function encryptString(string, username){
+  var user = users[username];
+  if (user){
+    
+  }
+}
+
+function decryptString(string, username){
+  
+}
+
 chrome.browserAction.onClicked.addListener(function(tab) {
   // Send a message to the active tab
   chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
     var activeTab = tabs[0];
     chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    
   });
 });
--- data/popup.js
+++ data/popup.js
@@ -1,6 +1,15 @@
-console.log("Hello");
-console.log("foo");
-console.log("bar");
 //chrome.runtime.sendMessage({hello: "bar"}, function(response){
 //  console.log(response);
 //});
+
+$(document).ready(function(){
+  $("#passphrase").click(function(){
+    $("#message").html("<input type=\"password\" id=\"entry\">");
+    $("#entry").keyup(function(e){
+      if (e.keyCode == 13){
+        
+      }
+    });
+
+  });
+});
