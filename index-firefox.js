e797fbe10f8e5379ba827dc88aa23c7403181367
cheekyshroom <phernima@ucsc.edu>
2016-01-29 17:37:19 -0800

    fixed names for chrome compat

---
+++ index-firefox.js
@@ -0,0 +1,24 @@
+var self = require('sdk/self');
+var buttons = require('sdk/ui/button/action');
+var tabs = require("sdk/tabs");
+var pageMod = require("sdk/page-mod");
+var ss = require("sdk/simple-storage");
+
+var button = buttons.ActionButton({
+  id: "mozilla-link",
+  label: "FBSecure",
+  icon: "./icon-16.png",
+  onClick: handleClick,
+});
+
+function handleClick(state){
+  console.log("clicked");
+}
+
+//Program starts at startup here:
+pageMod.PageMod({
+  include: "*.facebook.com",
+  contentScriptFile: [self.data.url("jquery.js"), self.data.url("main.js")],
+});
+
+console.log("Run######################################################");
