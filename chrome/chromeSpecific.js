3094e91368e84191ace726c07c6b4f2d94c0199d
Cole Faust <colecfaust@gmail.com>
2016-01-29 17:40:44 -0800

    moved chrome files around so it actually works

---
+++ chrome/chromeSpecific.js
@@ -0,0 +1,8 @@
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+
+      console.log("Button clicked!");
+    }
+  }
+);
--- data/main.js
+++ data/main.js
@@ -1,4 +1,6 @@
+console.log("RUUUUN");
 function run() {
+  console.log("run");
   $('#u_0_1m > .fbNub').each(function() {
     console.log($(this).find('.name').text());
   });
