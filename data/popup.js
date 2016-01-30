4dfbe777033568393477fabd404265bbe14c2e84
cheekyshroom <phernima@ucsc.edu>
2016-01-30 11:00:33 -0800

    Making passwords work

--- data/popup.js
+++ data/popup.js
@@ -1,15 +1,16 @@
-//chrome.runtime.sendMessage({hello: "bar"}, function(response){
-//  console.log(response);
-//});
 
 $(document).ready(function(){
-  $("#passphrase").click(function(){
+  $("#password").click(function(){
     $("#message").html("<input type=\"password\" id=\"entry\">");
     $("#entry").keyup(function(e){
       if (e.keyCode == 13){
-        
+        chrome.runtime.sendMessage({option: "set_password", data: $("#entry").val()}, function(response){
+          console.log("response: "+response);
+        });
+        $("#message").html("<input 
       }
     });
 
   });
+
 });
